610a0c5e6365a53657f7b9ffea334472b8ee88a3
James Prior <jep200404@columbus.rr.com>
2014-11-01 18:34:25 -0400

    largest_factor.py: Put real code in largest_factor() to make it pass all the tests.

--- largest_factor.py
+++ largest_factor.py
@@ -1,4 +1,14 @@
 import pytest
 
 def largest_factor(x):
-    return
+    if x <= 1 or divmod(x, 1)[1] != 0:
+        raise ValueError
+
+    divisor = 2
+    while x > 1:
+        quotient, remainder = divmod(x, divisor)
+        if remainder == 0:
+            x = quotient
+        else:
+            divisor += 1
+    return divisor
